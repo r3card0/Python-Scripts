4ab3506d8e8640d313822b4a135c956ebeba1ced
r3card0 <ricardo.rdz@pm.me>
2022-01-13 23:02:48 -0600

    create a currency converter from local currency to dollars

--- Python_Basic_Course/challenge01.py
+++ Python_Basic_Course/challenge01.py
@@ -4,5 +4,5 @@
 pesos = input("Please, enter your amount in pesos mexicanos: ")
 pesos = float(pesos) # convert to float
 dollar_price = 21.5
-dollar = str(round(pesos / dollar_price,2)) # convert to string and round 2 decimals
+dollar = str(round(pesos / dollar_price,2)) # convert to string and round 2 decimals.
 print("You have $" + str(dollar) + " dollars")

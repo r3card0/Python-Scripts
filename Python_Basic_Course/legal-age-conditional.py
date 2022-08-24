2f009687bb10a859e61f106dab2fdfdb0c6ddaa7
Ricardo Saldívar <95096808+r3card0@users.noreply.github.com>
2022-08-24 13:27:53 -0500

    Delete Python_Basic_Course directory

--- Python_Basic_Course/legal-age-conditional.py
+++
@@ -1,8 +0,0 @@
-# program to learn the usage of conditional: if, elif and else
-# this program was created in the course
-
-age = int(input("How old are you?: ")) # convert in integer the input
-if age > 17:
-    print("You are legal age")
-else:
-    print("You are not legal age")
--- Python_Basic_Course/planet_len_list.py
+++
@@ -1,7 +0,0 @@
-def run():
-    planetas = ['Mercurio','Venus','Tierra','Marte','Júpiter','Saturno','Urano','Neptuno']
-    print(len(planetas))
-
-
-if __name__ == '__main__':
-    run()

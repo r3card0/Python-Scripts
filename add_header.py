3914435c2162042c051873444427011e8b2d925a
Ricardo Saldivar <ricardo.rdz@pm.me>
2022-08-26 17:11:08 -0500

    fixed the column header Income and Area Code

--- add_header.py
+++ add_header.py
@@ -2,7 +2,7 @@ import pandas as pd
 from openpyxl.workbook import workbook
 
 def add_hdr():
-    column_names = ['First','Last', 'Address','City','State','Area','Code']
+    column_names = ['First','Last', 'Address','City','State','Area Code','Income']
     df_csv = pd.read_csv('Exercise_Files/Names.csv', header=None)
     df_csv.columns = column_names
 

ed5308e066cea4a4d39338bd4e356468e0311e93
Ricardo Saldivar <ricardo.rdz@pm.me>
2022-08-30 10:09:24 -0500

    Is not ready. Is not creating a new df

--- drop_columns.py
+++ drop_columns.py
@@ -2,12 +2,10 @@ import pandas as pd
 from openpyxl.workbook import workbook
 from add_header import add_hdr
 
-
-
 def description():
     program_description ="""
-    This program shows how to drop a set of columns to create a new 
-    dataset.
+    This program shows How to drop columns creating a list
+    and create a new dataset.
     df.drop(columns=, inplace=True)
     """
     return program_description
@@ -16,16 +14,23 @@ def description():
 df = add_hdr()
 
 # Create a list of columns. This list will be dropped from df
-col_list = df.columns
+col_list = list(df.columns)
 
 # Create a list from a selection
 def drop_list():
-    to_drop = ['Area Code','First','Address']
+    to_drop = ['Area Code','First']
     return to_drop
+
+def new_df():
+    to_drop = drop_list()
+    new_df = df.drop(columns=to_drop,inplace=True)
+    return new_df
 
 def run():
     print(description())
+    print(col_list)
     print(drop_list())
+    print(new_df())
 
 
 if __name__ == "__main__":

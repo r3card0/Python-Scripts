21c57f8d528ef65d061809cd22ed2b691355dd63
Ricardo Saldivar <ricardo.rdz@pm.me>
2022-08-30 08:45:37 -0500

    Ready! It can change the columns and conditions internally

--- filter_by_two_properties.py
+++ filter_by_two_properties.py
@@ -10,10 +10,33 @@ def description():
     """
     return program_description
 
+# Import dataframe df
 df = add_hdr()
+
+# Run first condition. List columns
+def col_list():
+    column_list = list(df.columns)
+    return column_list
+
+def choose_column01():
+    col_01 = 'City'
+    return col_01
+
+def choose_column02():
+    col_01 = 'First'
+    return col_01
+
+# Filter
+def filter_data():
+    col_01 = choose_column01()
+    col_02 = choose_column02()
+    data = df.loc[(df[col_01] == 'Riverside') & (df[col_02] == 'John')]
+    return data 
 
 def run():
     print(description())
+    print(col_list())
+    print(filter_data())
 
 if __name__ == "__main__":
     run()

1489374568806781b80df81242a8ac336da22c2e
Ricardo Saldivar <ricardo.rdz@pm.me>
2022-09-30 17:48:18 -0500

    added flag

--- improve_txt.py
+++ improve_txt.py
@@ -1,9 +1,12 @@
+# This program improves the format of a txt file
+# Uses flag delimiter=
+
 import pandas as pd
 from openpyxl.workbook import workbook
-from read_txt import read_txt
+#from read_txt import read_txtf
 
 def tab_delimiter():
-    text_file = read_txt()
+    #text_file = read_txtf()
     df_txt = pd.read_csv('Exercise_Files/data.txt', delimiter='\t')
 
     return df_txt
